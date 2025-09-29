4840eefb6c7978cadf7cd259eb623f662e6a9b81
adufou <antoine.du.fou@gmail.com>
2025-09-29 22:44:33 +0200

    feat: mobs move to player

--- modules/mob/mob.gd
+++ modules/mob/mob.gd
@@ -1,1 +1,20 @@
 extends PhysicalBodyBase
+
+const DISTANCE_TO_SEE_PLAYER = 500.0
+
+func can_go_to_player():
+	var player = PlayerManager.player
+
+	var distance_to_player = global_position.distance_to(player.global_position)
+	if distance_to_player > DISTANCE_TO_SEE_PLAYER:
+		return false
+
+	if not can_reach(player.global_position):
+		return false
+
+	return true
+
+
+func move_to_player():
+	target_pos(PlayerManager.player.global_position)
+	
--- modules/physical_body_base/physical_body_base.gd
+++ modules/physical_body_base/physical_body_base.gd
@@ -1,2 +1,29 @@
 extends RigidBody2D
 class_name PhysicalBodyBase
+
+var movement_speed = 100
+
+func _physics_process(_delta: float) -> void:
+	move_to_target()
+
+
+func can_reach(pos: Vector2) -> bool:
+	var map_rid = get_world_2d().navigation_map
+	var closest_point = NavigationServer2D.map_get_closest_point(map_rid, pos)
+	# If the closest point on the nav mesh is very close to the position, it's reachable.
+	return pos.distance_to(closest_point) < 1.0
+
+
+func target_pos(pos: Vector2) -> void:
+	%NavigationAgent2D.target_position = pos
+
+
+func move_to_target() -> void:
+	if %NavigationAgent2D.is_navigation_finished():
+		linear_velocity = Vector2.ZERO
+		return
+
+	var next_path_position: Vector2 = %NavigationAgent2D.get_next_path_position()
+	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_speed
+	# Using linear_velocity for RigidBody2D
+	linear_velocity = new_velocity
