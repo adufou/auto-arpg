a46c765788f63e482bbe60a4ed7f2e7514aac210
adufou <antoine.du.fou@gmail.com>
2025-09-30 13:23:25 +0200

    feat: when close enough mobs can attack (print)

--- modules/mob/mob.gd
+++ modules/mob/mob.gd
@@ -1,6 +1,7 @@
 extends PhysicalBodyBase
 
 const DISTANCE_TO_SEE_PLAYER = 500.0
+const DISTANCE_TO_ATTACK_PLAYER = 100.0
 
 func can_go_to_player():
 	var player = PlayerManager.player
@@ -14,7 +15,20 @@ func can_go_to_player():
 
 	return true
 
-
 func move_to_player():
-	target_pos(PlayerManager.player.global_position)
-	
+	target_pos(PlayerManager.player.global_position)	
+
+func can_attack_player():
+	var player = PlayerManager.player
+
+	var distance_to_player = global_position.distance_to(player.global_position)
+	if distance_to_player > DISTANCE_TO_ATTACK_PLAYER:
+		return false
+
+	if not can_reach(player.global_position):
+		return false
+
+	return true
+
+func attack_player():
+	print_debug("Mob at pos " + str(global_position) + " attacks player at pos " + str(PlayerManager.player.global_position))
